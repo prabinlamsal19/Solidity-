641d52a0e2b83c51ebc1fa1654e04830e431458d
prabinlamsal19 <lamsalprabin74@gmail.com>
2023-02-13 08:38:12 +0545

    initial commit

--- IfElseAndLoop.sol
+++ IfElseAndLoop.sol
@@ -5,6 +5,23 @@ contract MyContract{
     //conditionals
     //loops
     uint[] public numbers = [1,2,3,4,5,6,7,8,9,10];
+    address public owner;
+
+    constructor(){ 
+        owner=msg.sender;   //owner is a state variable
+    }
+
+    function countEvenNumbers() public view returns (uint){
+        uint count=0;
+        for (uint i=0; i<numbers.length; i++) 
+        {
+            if(isEven(numbers[i])){ 
+                count ++;
+            }
+            
+        }
+        return count ;
+    }
 
     function isEven(uint _number) public view returns(bool){ 
         if(_number %2 ==0){ 
@@ -12,6 +29,16 @@ contract MyContract{
         }
         else
             return false;
+    }
+
+
+
+    function addNumbers (uint _number) public {
+        numbers.push(_number);
+    }
+
+    function isOwner() public view returns(bool){ 
+        return(msg.sender == owner);
     }
 
 }
